fdb70df36971085bbf5d5a4d41b513a5026ca37b
puneetmanghwani <44107608+puneetmanghwani@users.noreply.github.com>
2019-01-17 13:17:09 -0500

    Add files via upload
    using webscraping and nltk tokenization to separate words and sentences

---
+++ rose.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+from bs4 import BeautifulSoup
+import requests
+import csv
+import nltk
+
+source = requests.get('http://en.wikipedia.org/wiki/Rose').text
+
+soup = BeautifulSoup(source, 'lxml')
+
+#csv_file = open('cms_scrape.csv', 'w')
+
+#csv_writer = csv.writer(csv_file)
+#csv_writer.writerow(['headline', 'summary', 'video_link'])
+data=""
+for p in soup.select('p'):
+  data=data+p.text
+  
+
+from nltk.tokenize import sent_tokenize
+from nltk.tokenize import word_tokenize
+
+sent_token = sent_tokenize(data)
+word_token = word_tokenize(data)
+
+print(sent_token)
+
+print("_______________&**************(^&*!^(*!&(*@!&#!@*#(@!&#()@!&#(*&@!#(@!________________")
+
+print(word_token)
+
+
+
