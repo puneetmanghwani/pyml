c7bc92a8efa71f301378c50831d0d5464bd9ee89
puneetmanghwani <puneetmanghwani3@gmail.com>
2019-01-27 03:18:09 -0500

    speech to text

---
+++ speech_to_text.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import speech_recognition as sr
+r = sr.Recognizer()
+
+
+mic=sr.Microphone()
+
+with mic as source:
+		
+	audio=r.listen(source)
+	
+print("you said", r.recognize_google(audio))
+
